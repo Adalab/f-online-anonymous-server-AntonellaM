3f5a288de3d7b6ee0ede0778da4b909eeba9c469
Antonella Michelli <antonella.michelli@upplication.com>
2019-07-04 13:46:17 +0200

    Add transition

--- js/main.js
+++ js/main.js
@@ -2,18 +2,19 @@
 
 const menuButton = document.querySelector('.nav-trigger');
 const closeButton = document.querySelector('.nav-close');
-const navigationMenu = document.querySelector('.nav');
+const navigationMenu = document.querySelector('.nav__items');
 const pageWrapperElement = document.querySelectorAll('.page__wrapper');
-
-
 menuButton.innerHTML = '';
 closeButton.innerHTML = '';
-navigationMenu.classList.add('hidden');
+closeButton.classList.add('hidden');
 pageWrapperElement[1].classList.add('page__wrapper--footer');
 
 function accessMenu() {
-    navigationMenu.classList.toggle('hidden');
+    console.log('CLICK')
+    navigationMenu.classList.toggle('active');
     menuButton.classList.toggle('hidden');
+    closeButton.classList.toggle('hidden');
+
 }
 
 menuButton.addEventListener('click', accessMenu);
