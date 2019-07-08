23fd98f251bb0363ead3fca3a85c0b0865302672
Antonella Michelli <antonella.michelli@upplication.com>
2019-07-08 14:07:32 +0200

    Slight refactorization: elliminate redundant js commands

--- js/main.js
+++ js/main.js
@@ -5,15 +5,11 @@ const closeButton = document.querySelector('.nav-close');
 const navigationMenu = document.querySelector('.nav__items');
 const pageWrapperElement = document.querySelectorAll('.page__wrapper');
 
-menuButton.innerHTML = '';
-closeButton.innerHTML = '';
-closeButton.classList.add('hidden');
 pageWrapperElement[1].classList.add('page__wrapper--footer');
 
 function accessMenu() {
     navigationMenu.classList.toggle('active');
     menuButton.classList.toggle('hidden');
-    closeButton.classList.toggle('hidden');
 }
 
 menuButton.addEventListener('click', accessMenu);
